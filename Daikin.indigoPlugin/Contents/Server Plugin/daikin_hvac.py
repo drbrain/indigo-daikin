6f3d7044a66659fe7134c2dcc370abb181afda40
Eric Hodel <drbrain@segment7.net>
2020-02-23 10:56:40 -0800

    Daikin thermostat plugin that is read-only

---
+++ Daikin.indigoPlugin/Contents/Server Plugin/daikin_hvac.py
@@ -0,0 +1,128 @@
+import urllib
+import urllib2
+
+class DaikinHVAC:
+    DIRECTIONS = {
+        "0": "stop",
+        "1": "vertical",
+        "2": "horizontal",
+        "3": "both",
+    }
+
+    MODES = {
+        "0": "automatic",
+        "1": "automatic",
+        "2": "dry",
+        "3": "cool",
+        "4": "heat",
+        "6": "fan",
+        "7": "automatic",
+    }
+
+    SPEEDS = {
+        "A": "automatic",
+        "B": "silent",
+        "3": "1",
+        "4": "2",
+        "5": "3",
+        "6": "4",
+        "7": "5",
+    }
+
+    def __init__(self, address):
+        self.address = address
+
+    def basic_info(self):
+        url = "http://%s/common/basic_info" % self.address
+
+        fields = self.__GET(url)
+
+        basic_info = {}
+
+        basic_info["name"]  = urllib.parse.unquote(fields["name"])
+        basic_info["power"] = fields["pow"] == "1"
+
+        return basic_info
+
+    def control_info(self):
+        url = "http://%s/aircon/get_control_info" % self.address
+
+        fields = self.__GET(url)
+
+        control_info = {}
+
+        control_info["power"]        = fields["pow"] == "1"
+        control_info["mode"]         = self.MODES[fields["mode"]]
+        control_info["setPoint"]     = float(fields["stemp"])
+        control_info["setHumidity"]  = float(fields["shum"])
+        control_info["fanSpeed"]     = self.SPEEDS[fields["f_rate"]]
+        control_info["fanDirection"] = self.DIRECTIONS[fields["f_dir"]]
+
+        control_info["setpointCool"]     = float(fields["dt3"])
+        control_info["setpointHeat"]     = float(fields["dt4"])
+        control_info["setpointHeatCool"] = float(fields["dt7"])
+
+        return control_info
+
+    def sensor_info(self):
+        url = "http://%s/aircon/get_sensor_info" % self.address
+
+        fields = self.__GET(url)
+
+        sensor_info = {}
+
+        if fields["hhum"] == "-":
+            sensor_info["humidity"] = None
+        else:
+            sensor_info["humidity"] = float(fields["hhum"])
+
+        if fields["htemp"] == "-":
+            sensor_info["temperature"] = None
+        else:
+            sensor_info["temperature"] = float(fields["htemp"])
+
+        if fields["otemp"] == "-":
+            sensor_info["outdoorTemperature"] = None
+        else:
+            sensor_info["outdoorTemperature"] = float(fields["otemp"])
+
+        return sensor_info
+
+    def __GET(self, url):
+        try:
+            f = urllib2.urlopen(url)
+        except urllib2.HTTPError, e:
+            self.errorLog('Error fetching %s: %s' % (url, str(e)))
+            return;
+
+        fields = split(f.read, ",")
+
+        f.close()
+
+        response = {}
+
+        for field in fields:
+            key, value = split(field, "=", 2)
+            response[key] = value
+
+        return response
+
+    def __POST(self, url, data):
+        request = urllib2.Request(url, data=body)
+        request.add_header('Content-Type', 'application/json')
+        request.get_method = lambda: "POST"
+
+        opener = urllib2.build_opener(urllib2.HTTPHandler)
+
+        try:
+            f = opener.open(request)
+        except urllib2.HTTPError, e:
+            self.errorLog('Error fetching %s: %s' % (url, str(e)))
+            return;
+
+        response = json.load(f)
+
+        f.close()
+
+        return response
+
---
+++ Daikin.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import indigo
+
+class Plugin(indigo.PluginBase):
+    def __init__(self, pluginId, pluginDisplayName, pluginVersion,
+                 pluginPrefs):
+        indigo.PluginBase.__init__(self, pluginId, pluginDisplayName,
+                                   pluginVersion, pluginPrefs)
+
+        self.debug = pluginPrefs.get('debug', False)
+
+        self.devices = {}
+
+    def deviceStartComm(self, device):
+        if device.id not in self.devices:
+            self.devices[device.id] = device
+            self.update(device)
+
+    def deviceStopComm(self, device):
+        if device.id in self.devices:
+            self.devices.pop(device.id)
+
+    def update(self, device):
+        if device.deviceTypeId == "DaikinHVACUnit":
+            self.updateHVAC(device)
+
+    def updateHVAC(self, device):
+        self.debugLog("Updating HVAC %s" % device.address)
+
+        api = DaikinHVAC(device.address)
+
+        sensor_info = api.sensor_info()
+
+        if sensor_info["humidity"]:
+            device.updateStateOnServer("humidityInput0", sensor_info["humidity"])
+        device.updateStateOnServer("outdoor_temperature", sensor_info["outdoor_temperature"])
+        if sensor_info["temperature"]:
+            device.updateStateOnServer("temperatureInput0", sensor_info["temperature"])
+        device.updateStateOnServer("outdoorTemperature", sensor_info["outdoorTemperature"])
+
+        control_info = api.control_info()
+
+        device.updateStateOnServer("fanDirection", control_info["fanDirection"])
+        device.updateStateOnServer("fanSpeed",     control_info["fanSpeed"])
+
+        dehumidifierOn = False
+        fanMode        = indigo.kFanMode.Auto
+        operationMode  = indigo.kHvacMode.Off
+        setpointCool   = control_info["setpointCool"]
+        setpointHeat   = control_info["setpointHeat"]
+
+        if control_info["power"]:
+            mode = control_info["mode"]
+
+            if mode == "automatic":
+                operationMode = indigo.kHvacMode.ProgramHeatCool
+                setpointCool  = control_info["setpointHeatCool"]
+                setpointHeat  = control_info["setpointHeatCool"]
+            else if mode == "cool":
+                operationMode = indigo.kHvacMode.ProgramCool
+            else if mode == "heat":
+                operationMode = indigo.kHvacMode.ProgramHeat
+            else if mode == "dry":
+                dehumidifierOn = True
+            else if mode == "fan":
+                fanMode       = indigo.kFanMode.AlwaysOn
+                operationMode = indigo.kHvacMode.Off
+
+        device.updateStateOnServer("hvacDehumidifierIsOn", dehumidifierOn)
+        device.updateStateOnServer("hvacFanMode", fanMode)
+        device.updateStateOnServer("hvacOperationMode", operationMode)
+        device.updateStateOnServer("setpointCool", setpointCool)
+        device.updateStateOnServer("setpointHeat", setpointHeat)
+
