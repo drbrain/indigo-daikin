eb77292b46a3b162026a68146e60e7b881129a2e
Eric Hodel <drbrain@segment7.net>
2020-02-23 15:28:43 -0800

    Set outdoor temperature to None if not present

--- Daikin.indigoPlugin/Contents/Server Plugin/plugin.py
+++ Daikin.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -70,8 +70,12 @@ class Plugin(indigo.PluginBase):
         if sensor_info["temperature"]:
             device.updateStateOnServer("temperatureInput1", self.convertTemperature(device, sensor_info["temperature"]))
 
+        outdoorTemperature = None
+
         if sensor_info["outdoorTemperature"]:
-            device.updateStateOnServer("outdoorTemperature", self.convertTemperature(device, sensor_info["outdoorTemperature"]))
+            outdoorTemperature = self.convertTemperature(device, sensor_info["outdoorTemperature"])
+
+        device.updateStateOnServer("outdoorTemperature", outdoorTemperature)
 
         control_info = api.control_info()
 
